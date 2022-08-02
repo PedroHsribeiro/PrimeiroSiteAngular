06dfba3c169a0e6da8b2594c5b7f1dca8a4e7240
Pedro <pedro.hsribeiro@hotmail.com>
2022-08-01 21:27:30 -0300

    atualização do script do carrossel

--- src/assets/js/scripts.js
+++ src/assets/js/scripts.js
@@ -1,3 +1,5 @@
+const { escapeSelector } = require("jquery");
+
 $(document).ready(function () {
 
     var isLargeWindow;
@@ -51,82 +53,91 @@ $(document).ready(function () {
 
 });
 
-function mostrarMenu() {
-    let displayMenu = document.getElementById("menu");
+window.addEventListener("resize", mostrarItens)
 
-    if (getComputedStyle(displayMenu).display == 'none') {
-        displayMenu.style.display = 'flex';
-    } else {
-        displayMenu.style.display = 'none';
-    }
-
-}
-
-function mostrarPesquisa() {
-    let displayPesquisa = document.getElementById("search-box");
-
-    if (window.innerWidth < 1024) {
-        if (getComputedStyle(displayPesquisa).display == 'none') {
-            if (window.innerWidth < 560) {
-                displayPesquisa.style.display = 'flex';
-                document.getElementById("icon-logo").style.display = 'none';
-            } else {
-                displayPesquisa.style.display = 'flex';
-            }
-        } else {
-            if (window.innerWidth < 560) {
-                document.getElementById("icon-logo").style.display = 'flex';
-                displayPesquisa.style.display = 'none';
-            } else {
-                displayPesquisa.style.display = 'none';
-            }
-        }
-    }
-}
 function mostrarItens() {
     if (window.innerWidth > 1024) {
         document.getElementById("menu").style.display = 'flex'
         document.getElementById("search-box").style.display = 'flex'
-    } else {
-        document.getElementById("menu").style.display = 'none'
-        document.getElementById("search-box").style.display = 'none'
-    }
-}
-
-const slides = document.querySelectorAll("banner-home")
-document.getElementById("banner_button--next").addEventListener("click", nextSlide)
-document.getElementById("banner_button--prev").addEventListener("click", prevSlide)
-
-const lastSlideIndex = slides.length - 1
-let currentSlideIndex = 0
-
-function nextSlide(){
-
-    if (currentSlideIndex === lastSlideIndex) {
-        currentSlideIndex = 0
-    } else {
-        currentSlideIndex++
     }
 
-    slides.forEach(slide => {
-        slide.classList.remove('bannerVisibility')
-    })
+}   
 
-    slides[currentSlideIndex].classList.add('bannerVisibility')
+window.addEventListener('load', (event) => {
 
-}
+    document.getElementById('menu-icon').addEventListener('click', mostrarMenu)
 
-function prevSlide(){
+    function mostrarMenu() {
+        let displayMenu = document.getElementById("menu");
 
-    if (currentSlideIndex === 0) {
-        currentSlideIndex = lastSlideIndex
-    } else {
-        currentSlideIndex--
+        if (getComputedStyle(displayMenu).display == 'none') {
+            displayMenu.style.display = 'flex';
+        } else {
+            displayMenu.style.display = 'none';
+        }
+
     }
 
-    slides.forEach(slide => {
-        slide.classList.remove('bannerVisibility')
-    })
+    var displayPesquisa = document.getElementById("search-box");
+    document.getElementById('search-icon').addEventListener('click', mostrarPesquisa)
 
-    slides[currentSlideIndex].classList.add('bannerVisibility')
-}
+    function mostrarPesquisa() {
+        if (window.innerWidth < 1024) {
+            if (getComputedStyle(displayPesquisa).display == 'none') {
+                if (window.innerWidth < 560) {
+                    displayPesquisa.style.display = 'flex';
+                    document.getElementById("icon-logo").style.display = 'none';
+                } else {
+                    displayPesquisa.style.display = 'flex';
+                }
+            } else {
+                if (window.innerWidth < 560) {
+                    document.getElementById("icon-logo").style.display = 'flex';
+                    displayPesquisa.style.display = 'none';
+                } else {
+                    displayPesquisa.style.display = 'none';
+                }
+            }
+        }
+    }
+
+
+    const slides = document.querySelectorAll('[data-js="banner-home"]')
+    document.getElementById("banner_button--next").addEventListener("click", nextSlide)
+    document.getElementById("banner_button--prev").addEventListener("click", prevSlide)
+
+
+    const lastSlideIndex = slides.length - 1
+    let currentSlideIndex = 0
+
+    function nextSlide() {
+
+        if (currentSlideIndex === lastSlideIndex) {
+            currentSlideIndex = 0
+        } else {
+            currentSlideIndex++
+        }
+
+        slides.forEach(slide => {
+            slide.classList.remove('bannerVisibility')
+        })
+
+        slides[currentSlideIndex].classList.add('bannerVisibility')
+
+    }
+
+    function prevSlide() {
+
+        if (currentSlideIndex === 0) {
+            currentSlideIndex = lastSlideIndex
+        } else {
+            currentSlideIndex--
+        }
+
+        slides.forEach(slide => {
+            slide.classList.remove('bannerVisibility')
+        })
+
+        slides[currentSlideIndex].classList.add('bannerVisibility')
+    }
+})
